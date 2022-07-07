3a9b19756ab2c370b879f4b3e48e2758515b5cc8
Danyyl Dolynsky <dolynskydanyyl@gmail.com>
2022-07-07 18:14:05 +0300

    First version

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,8 +1,9 @@
 from unittest import TestCase, main
 import anagrams
 
+
 class TypycalAnagramsTest(TestCase):
-    
+
     def test_string(self):
         self.assertEqual(isinstance(anagrams.reverse_text(''), str), True)
         self.assertEqual(isinstance(anagrams.reverse_word(''), str), True)
@@ -20,15 +21,13 @@ class TypycalAnagramsTest(TestCase):
         for text, reversed_text in cases:
             self.assertEqual(anagrams.reverse_text(text), reversed_text)
 
+
 class AtypycalAnagramsTest(TestCase):
 
     def test_string(self):
         with self.assertRaises(ValueError) as e:
             anagrams.reverse_text(4)
         self.assertEqual(('Вираз повинен бути типу string', e.exception.args[0]))
-
-
-
 
 
 if __name__ == '__main__':
